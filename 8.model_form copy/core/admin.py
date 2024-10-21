7e013eab818146f9f3d42a14c9c59dfff5a6a0c7
Sandip <jaiswarsandeep00@gmail.com>
2024-10-21 12:47:04 +0530

    update

--- 8.model_form copy/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import Marvel
-# Register your models here.
-
-@admin.register(Marvel)
-class MarvelAdmin(admin.ModelAdmin):
-    list_display =['name','heroic_name']
--- 8.model_form copy/core/forms.py
+++
@@ -1,8 +0,0 @@
-from  django import forms
-from . models import Marvel
-
-class MarvelForm(forms.ModelForm):
-    
-    class Meta:
-        model = Marvel
-        fields = ['name','heroic_name']
